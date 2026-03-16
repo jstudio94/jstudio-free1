e09fa8d70b6cd97a6b4f333777eeada3d6f8bba8
unknown <backgroun94@gmail.com>
2026-03-16 20:46:37 +0900

    비디오 렌더링(ffmpeg) 빌드 에러 해결

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  // ✅ Vercel 빌드 에러 해결: "비디오 관련 무거운 프로그램들은 포장(검사)하지 말고 그냥 넘어가!"
+  serverExternalPackages: ['@ffmpeg-installer/ffmpeg', 'fluent-ffmpeg'],
 };
 
 export default nextConfig;
