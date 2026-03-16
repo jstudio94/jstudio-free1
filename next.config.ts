af273255f22b937f07d4d8df314c5e6443854867
unknown <backgroun94@gmail.com>
2026-03-16 20:26:21 +0900

    경고 문구 제거

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ESLint(문법 검사) 에러가 있어도 무시하고 배포
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   // TypeScript(타입 검사) 에러가 있어도 무시하고 배포
   typescript: {
     ignoreBuildErrors: true,
